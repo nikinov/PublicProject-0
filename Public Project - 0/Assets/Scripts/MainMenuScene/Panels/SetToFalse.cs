37f476c8b461ef60e3b1c9c7883cff608bf24df6
nikinov <novellenicholas@gmail.com>
2019-09-18 16:36:19 +0200

    Script to Set Object felse and bug fixes

---
+++ Public Project - 0/Assets/Scripts/MainMenuScene/Panels/SetToFalse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetToFalse: MonoBehaviour
+{
+    void Start()
+    {
+        GameObject[] gameObjects;
+        gameObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+        gameObject.SetActive(false);
+
+    }
+}
